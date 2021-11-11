30c42a828faf0f7bbea22bee948b0e9f87cbc1b5
BILGIN <busra2000@live.dk>
2021-11-11 12:58:57 +0100

    busra

--- src/main/java/Aftale/Aftale.java
+++ src/main/java/Aftale/Aftale.java
@@ -3,26 +3,7 @@ package Aftale;
 import java.util.Date;
 
 public class Aftale {
-
-    private Date date;
-    private String cpr;
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getCpr() {
-        return cpr;
-    }
-
-    public void setCpr(String cpr) {
-        this.cpr = cpr;
-    }
-
-
-
+    Patient patient;
+    Date tidspunkt;
+    String notat;
 }
